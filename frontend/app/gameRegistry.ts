3c1e0cc7970e94267f96a029e7a382922aa99dea
aanand1164 <arshiyaanand5@gmail.com>
2026-05-11 01:10:45 +0530

    Connected more games to the application

--- frontend/app/gameRegistry.ts
+++ frontend/app/gameRegistry.ts
@@ -1,7 +1,11 @@
 import MatchTiles from "../games/tapthe_Matching/App"
 import ScavengerHunt from "../games/Scavengerhunt/App"
+import EmotionGame from "../games/emotiongame"
+import MemoryGame from "../games/memoryfam"
 
 export const gameRegistry = {
   Match_the_Tiles: MatchTiles,
   Scavenger_Hunt: ScavengerHunt,
+  Emotion_Game: EmotionGame,
+  Memory_Fam: MemoryGame,
 }
